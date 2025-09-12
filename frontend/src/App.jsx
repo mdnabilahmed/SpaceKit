d552edf9a54cd2af3dce6adef90d998c8de154a2
mdnabilahmed <mdnabilahmed706@gmail.com>
2025-09-12 22:11:46 +0530

    added lazy load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
+import React from "react";
 
-
-import Home from "../pages/Home";
-import AddProduct from "../pages/AddProduct";
-import Login from "../pages/Login";
+const Home = React.lazy(() => import("../pages/Home"));
+const AddProduct = React.lazy(() => import("../pages/AddProduct"));
+const Login = React.lazy(() => import("../pages/Login"));
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/addproduct" element={<AddProduct />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center h-screen">
+            <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin"></div>
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addproduct" element={<AddProduct />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
