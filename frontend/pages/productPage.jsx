a3596d89dcb1a55d1f12e70b3505fd204bc63a65
mdnabilahmed <mdnabilahmed706@gmail.com>
2025-10-08 19:30:04 +0530

    debugging

--- frontend/pages/productPage.jsx
+++ frontend/pages/productPage.jsx
@@ -44,7 +44,7 @@ const ProductPage = () => {
           ← Back to Products
         </Link>
 
-        <div className="max-w-8xl mx-auto ml-10">
+        <div className="max-w-8xl mx-auto">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16">
             {/* Left side - Images */}
             <div className="space-y-4">
